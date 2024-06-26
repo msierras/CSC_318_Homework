9c92cdaa5e98cdac04e229cd38b296354116c264
Melvin <melvin.sierraic3@gmail.com>
2024-06-26 18:41:12 -0400

    Adding onclick function calculateGPA() to submit button. Function will retrieve information from grade and credit text boxes to calculate GPA. Course text field is ignored as it will not be helpful in the calculation.

---
+++ homework_Two/scripts/index.js
@@ -0,0 +1,20 @@
+// Gets the values for grade that user input
+
+function calculateGPA() {
+    event.preventDefault(); // Prevents inputs from being deleted when submit button is pressed
+    let gpa; 
+
+    // These are the grade inputs 
+    let grade_One = document.getElementById("grade_One").value;
+    let grade_Two = document.getElementById("grade_Two").value;
+    let grade_Three = document.getElementById("grade_Three").value;
+    let grade_Four = document.getElementById("grade_Four").value;
+    let grade_Five = document.getElementById("grade_Five").value;
+    let grade_Six = document.getElementById("grade_Six").value;
+    let grade_Seven = document.getElementById("grade_Seven").value;
+    let grade_Eight = document.getElementById("grade_Eight").value;
+    
+    // These are the credit inputs 
+}
+
+
